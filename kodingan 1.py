1bc67751672ba3e76677e5c90567fbef0481d447
AndreanaWirawan <100249145+AndreanaWirawan@users.noreply.github.com>
2022-10-19 18:35:34 +0700

    Add files via upload

---
+++ kodingan 1.py
@@ -0,0 +1,15 @@
+nama = "I PUTU ANDREANA WIRAWAN"
+nim = "D0221068"
+agama = "hindu"
+alamat = "Topoyo"
+umur = "18"
+tinggi_badan = "1,8"
+ukuran_sepatu = "42"
+
+print("nama :"+nama)
+print("nim :"+nim)
+print("agama :"+agama)
+print("alamat :"+alamat)
+print("umur :"+umur)
+print("tinggi badan :"+tinggi_badan)
+print("ukuran sepatu :"+ukuran_sepatu)
