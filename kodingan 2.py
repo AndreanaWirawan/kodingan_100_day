50c9d0f76477e0d3e78729add1d00309beee292b
AndreanaWirawan <100249145+AndreanaWirawan@users.noreply.github.com>
2022-10-20 17:39:41 +0700

    Add files via upload

---
+++ kodingan 2.py
@@ -0,0 +1,14 @@
+print('=' * 20, "KODINGAN 2", '=' * 20)
+nama =(input("masukan nama anda :"))
+nim = "D0221068"
+agama = "hindu"
+jurusan = "Informatika"
+alamat = "Topoyo" 
+
+print()
+print("Nama :" +nama)
+print("Nim :" +nim)
+print("Agama :" +agama)
+print("Jurusan :" +jurusan)
+print("Alamat :" +alamat)
+
