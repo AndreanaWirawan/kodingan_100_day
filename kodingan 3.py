15d6231932c07e1cb8aa30f0780c7efe9fac8f9b
AndreanaWirawan <100249145+AndreanaWirawan@users.noreply.github.com>
2022-10-21 12:55:01 +0700

    Add files via upload

---
+++ kodingan 3.py
@@ -0,0 +1,13 @@
+print('=' * 20, "KODINGAN 3", '=' * 20)
+print()
+
+nilai1 = int(input("masukan angka anda :"))
+nilai2 = int(input("masukan angka anda :"))
+a = nilai1 < nilai2
+print(a)
+b = nilai1 > nilai2
+print(b)
+c = "nilai1" == "nilai2"
+print(c)
+
+
