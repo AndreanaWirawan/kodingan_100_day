7193d911c691146d5ee7b0b78c527df7a8953f9a
AndreanaWirawan <100249145+AndreanaWirawan@users.noreply.github.com>
2022-10-22 12:03:07 +0700

    Add files via upload

---
+++ kodingan 4.py
@@ -0,0 +1,13 @@
+print('=' * 20, "KODINGAN 4", '=' * 20)
+print()
+kode = str(input("masukan warna lampu : "))
+if kode == "merah":
+    print("stop")
+elif kode == "kuning":
+    print("hati-hati")
+elif kode == "
+hijau":
+    print("selamat jalan")
+else:
+    print("kode yang anda masukan salah")
+
