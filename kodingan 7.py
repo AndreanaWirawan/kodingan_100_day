a1830161dad60f63e856ae66ea9f4d0a32d6c79c
AndreanaWirawan <100249145+AndreanaWirawan@users.noreply.github.com>
2022-10-25 16:05:45 +0700

    Add files via upload

---
+++ kodingan 7.py
@@ -0,0 +1,11 @@
+print('=' * 20, "KODINGAN 5", '=' * 20)
+for i in range (10):
+    print(i)
+print()
+for i in range (2,10,2):
+    print(i)
+print()
+nama = ["putu","eksan"]
+for i in nama:
+    print(i)
+
