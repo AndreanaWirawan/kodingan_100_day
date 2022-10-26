0f88fd145cebbda236062825851f1f1106c2b84e
AndreanaWirawan <100249145+AndreanaWirawan@users.noreply.github.com>
2022-10-26 21:04:18 +0700

    Add files via upload

---
+++ kodingan 8.py
@@ -0,0 +1,14 @@
+nama_hewan = ['anjing','kucing','monyet','sapi']
+for i in nama_hewan:
+    print("ini adalah hewan :",i)
+
+print()
+angka = [1,2,3,4,5,6,7]
+for j in angka:
+    print("ini adalah angka :",j)
+print()
+for h in range(2,10,2):
+    print("ini adalah angka genap :",h)
+print()
+for m in range(3,20,3):
+    print("ini adalah angka ganjil :",m)
