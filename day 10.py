5747b729a6bc4cbfbbf834a278337ac3203c0305
AndreanaWirawan <100249145+AndreanaWirawan@users.noreply.github.com>
2022-10-28 09:31:58 +0700

    Add files via upload

---
+++ day 10.py
@@ -0,0 +1,23 @@
+print('=' * 20, "KODINGAN 10", '=' * 20)
+berat_badan = int(input("masukan berat badan anda :"))
+
+if berat_badan >= 10 and berat_badan <=20:
+    print("anda cocoknya menggukan baju ukuran : S")
+
+elif berat_badan >= 10 and berat_badan <=20:
+    print("anda cocoknya menggukan baju ukuran : S")
+
+elif berat_badan >=21 and berat_badan <=30:
+    print("anda cocoknya menggukan baju ukuran : M")
+
+elif berat_badan >= 31 and berat_badan <=45:
+    print("anda cocoknya menggukan baju ukuran : L")
+
+elif berat_badan >= 46 and berat_badan <=65:
+    print("anda cocoknya menggukan baju ukuran : XL")
+
+elif berat_badan >= 66 and berat_badan <=90:
+    print("anda cocoknya menggukan baju ukuran : XXL")
+    
+else:
+    print("maaf untuk ukuran baju yang anda masukan belum tersedia")
