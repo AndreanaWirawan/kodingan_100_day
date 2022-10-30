86499a0b4594b6227302f7bba8fc4ff098acedb6
AndreanaWirawan <100249145+AndreanaWirawan@users.noreply.github.com>
2022-10-30 18:04:44 +0700

    Add files via upload

---
+++ kodingan 12.py
@@ -0,0 +1,8 @@
+while True : 
+    variabel = input("masukan tipe data :")
+    print("ini adlah tipe data:",type(variabel))
+    hasil = input("lanjut or tidak? ")
+    if hasil == "tidak":
+        print("program selesai")
+        break
+    
