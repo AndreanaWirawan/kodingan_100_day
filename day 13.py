e90bf05316ee8592a48fa76a4706bc8bb16b3898
AndreanaWirawan <100249145+AndreanaWirawan@users.noreply.github.com>
2022-10-31 16:57:42 +0700

    Add files via upload

---
+++ day 13.py
@@ -0,0 +1,38 @@
+while True:
+    print()
+    masukan_hari = input("masukan hari apa sekarang?")
+    print()
+
+    if masukan_hari == "senin":
+        print("ingat hari ini ada dua tugas")
+        print("1.tugas basis data")
+        print("2.tugas pemrograman web")
+        print()
+
+    elif masukan_hari == "selasa":
+        print("sekarang tidak ada tugas anda bebas")
+        print()
+
+    elif masukan_hari == "rabu":
+        print("kamu nanti mid jam 02:00")
+        print("kamu mid bahasa indonesia oke")
+        print()
+
+    elif masukan_hari == "kamis":
+        print("hari ini kamu fainal Mata Kuliah python oke")
+        print()
+
+    elif masukan_hari == "jukmat":
+        print("sekarang libur tapi tugas ada")
+        print("1.tugas bahasa inggris")
+        print("2.tugas matematika")
+        print()
+
+    elif masukan_hari == "sabtu":
+        print("ingat bro hari ini deadline tugas basis data oke")
+        print()
+
+    else:
+        print("ini hari libur")
+        break
+    
