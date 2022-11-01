a4edc1a9055934d0855e2464e0842a920a0d2231
AndreanaWirawan <100249145+AndreanaWirawan@users.noreply.github.com>
2022-11-01 16:25:46 +0700

    Add files via upload

---
+++ day 14.py
@@ -0,0 +1,10 @@
+while True:
+    angka = float(input("masukan angka anda :"))
+    if angka > 0:
+        print("ini adalah angka positif")
+        print()
+    elif angka == 0:
+        print("angka Nol")
+        print()    
+    else:
+        print("ini adalah bilangan negatif")
