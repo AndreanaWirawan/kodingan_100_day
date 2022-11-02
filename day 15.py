4ebd190da4bbee0c61c2254d854a56dfc9d03525
AndreanaWirawan <100249145+AndreanaWirawan@users.noreply.github.com>
2022-11-02 20:04:24 +0700

    Add files via upload

---
+++ day 15.py
@@ -0,0 +1,4 @@
+kilometer = float(input("tulisakan jarak kilometer anda :"))
+faktor_konfersi = 0.621371
+mil = kilometer * faktor_konfersi
+print("%0.2f Kilometer sama dengan:%0.2f Mil"%(kilometer,mil))
