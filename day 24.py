0578a4c1ce5e4d2112847a45587725e588dab044
AndreanaWirawan <100249145+AndreanaWirawan@users.noreply.github.com>
2022-11-11 20:17:49 +0700

    Add files via upload

---
+++ day 24.py
@@ -0,0 +1,7 @@
+print("MEGKONVERSI WAKTU")
+detik = int(input("masukan detik yang ingin di konversi : "))
+menit = detik/60
+jam = detik/3600
+print(detik,'detik di rubah ke menit : ',menit,'menit')
+print()
+print(menit,'detik di rubah ke jam : ',jam,'jam')
