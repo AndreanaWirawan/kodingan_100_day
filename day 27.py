dfeee7e0df9a16da8cb84ebd303d64f6165789c2
AndreanaWirawan <100249145+AndreanaWirawan@users.noreply.github.com>
2022-11-14 18:03:13 +0700

    Add files via upload

---
+++ day 27.py
@@ -0,0 +1,8 @@
+print('UNTUK MENEMUKAN BILANGAN GANJIL YANG HABIS DI BAGI 3')
+end = int(input("masukan angka anda : "))
+for i in range(0, end+1):
+    if (i%3==0 and i%2==1):
+        print(i)   
+print('program selesai')     
+
+        
