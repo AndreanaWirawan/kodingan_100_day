438c7cdaf79d2316cbdf97e8465e39e7f6f9bd46
AndreanaWirawan <100249145+AndreanaWirawan@users.noreply.github.com>
2022-11-14 20:29:30 +0800

    Update day 27.py

--- day 27.py
+++ day 27.py
@@ -1,7 +1,7 @@
 print('UNTUK MENEMUKAN BILANGAN GANJIL YANG HABIS DI BAGI 3')
 end = int(input("masukan angka anda : "))
 list_kosong =[]
-for i in range(0, end+1):
+for i in range(1, end+1):
     if (i%3==0 and i%2==1):
         list_kosong.append(i)
 print(list_kosong)
