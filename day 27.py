0642c568bd4067b632d9963b2bdb421cf88ed958
AndreanaWirawan <100249145+AndreanaWirawan@users.noreply.github.com>
2022-11-14 19:05:48 +0700

    Add files via upload

--- day 27.py
+++ day 27.py
@@ -1,8 +1,12 @@
 print('UNTUK MENEMUKAN BILANGAN GANJIL YANG HABIS DI BAGI 3')
 end = int(input("masukan angka anda : "))
+list_kosong =[]
 for i in range(0, end+1):
     if (i%3==0 and i%2==1):
-        print(i)   
-print('program selesai')     
+        list_kosong.append(i)
+print(list_kosong)
+print("hasil keseluruhan :",sum(list_kosong))
+print('program selesai')   
+
 
         
