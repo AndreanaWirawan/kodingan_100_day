5308a93a572e4ed1f23aab41eaa0d424982b1a7a
AndreanaWirawan <100249145+AndreanaWirawan@users.noreply.github.com>
2022-11-23 16:47:16 +0700

    Add files via upload

---
+++ day 36.py
@@ -0,0 +1,10 @@
+print("menyilangkan hasil sebuah bilangan")
+angka1 = int(input("masukan nilai X : "))
+angka2 = int(input("masukan nilai Y : "))
+
+pertukaran = angka1
+angka1 = angka2
+angka2 = pertukaran
+
+print("hasil Y setelah di silang :",angka1)
+print("hasil X setelah di silang :",angka2)
