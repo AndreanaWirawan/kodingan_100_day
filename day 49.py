301e7810b143daa864e28eb3d6009d13fa41dc2e
AndreanaWirawan <100249145+AndreanaWirawan@users.noreply.github.com>
2022-12-06 19:37:21 +0700

    Add files via upload

---
+++ day 49.py
@@ -0,0 +1,8 @@
+print("menghitung luas segitiga siku siku")
+
+angka1 = float(input("masukan panjang alas : "))
+angka2 = float(input("masukan tinggi segitiga : "))
+
+luas = 0.5*angka1*angka2
+
+print("luas segitiga adalah : ",+luas,"cm")
