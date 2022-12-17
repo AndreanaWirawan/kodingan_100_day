3055ace42208c763c7f203244b49c09749be6bd8
AndreanaWirawan <100249145+AndreanaWirawan@users.noreply.github.com>
2022-12-17 19:04:53 +0800

    Add files via upload

---
+++ day 60.py
@@ -0,0 +1,19 @@
+class biodata:
+    nama = None
+    nim = None
+    agama = None
+    jurusan = None
+    alamat = None
+    
+biodata1 = biodata()
+biodata1.nama = 'putu'
+biodata1.nim = 'D0221068'
+biodata1.agama = 'hindu'
+biodata1.jurusan = 'informatika'
+biodata1.alamat = 'Topoyo'
+
+print('nama saya adalah :',biodata1.nama)
+print('nim saya adalah :',biodata1.nim)
+print('agama saya adalah :',biodata1.agama)
+print('jurusan saya adalah :',biodata1.jurusan)
+print('alamat saya adalah :',biodata1.alamat)
