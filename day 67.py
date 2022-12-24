aa2cedee45f160bcc17d4a0840ad079e296786ce
AndreanaWirawan <100249145+AndreanaWirawan@users.noreply.github.com>
2022-12-24 11:16:14 +0800

    Add files via upload

---
+++ day 67.py
@@ -0,0 +1,7 @@
+
+angka1 = int(input("Masukan angka awal : "))
+angka2 = int(input('Masukan angka akhir : '))
+angka3 = int(input('masukan angka lompatan : '))
+
+for i in range(angka1 ,angka2+1 , angka3):
+    print(i)
