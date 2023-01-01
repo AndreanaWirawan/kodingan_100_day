b904b067c2cea95c057d9a19a172b244ad89319d
AndreanaWirawan <100249145+AndreanaWirawan@users.noreply.github.com>
2023-01-01 08:52:29 +0800

    Add files via upload

---
+++ day 75.py
@@ -0,0 +1,21 @@
+print("TES BUAT KTP")
+def biodata():
+    nama = input("Masukan nama anda : ")
+    umur = input("Masukan umur anda : ")
+    jenis_kelamin = input("Masukan jenis kelamin : ")
+    alamat = input("Masukan alamat : ")
+    golongan_darah = input("Masukan golongan darah : ")
+    tempat_tanggal_lahir = input("Masukan tanggal lahir anda : ")
+    status = input("Masukan status anda : ")
+    print()
+              
+    print('Nama : ',nama)
+    print('Umur : ',umur)
+    print('jenis kelamin : ',jenis_kelamin)
+    print('Alamat : ',alamat)
+    print("Golongan darah : ",golongan_darah)
+    print("Tempat tanggal lahir :",tempat_tanggal_lahir)
+    print("status : ",status)
+
+
+biodata()
