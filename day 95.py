05fce7aa4a530e2de9e7a6fb8fd79bff87d78759
AndreanaWirawan <100249145+AndreanaWirawan@users.noreply.github.com>
2023-01-21 16:31:12 +0800

    Add files via upload

---
+++ day 95.py
@@ -0,0 +1,124 @@
+login = {}
+def menu():
+    print('jadwal matakuliah'.center(40, ' '))
+    print("Login".center(40, ' '))
+    user = 'user'
+    sandi = '123'
+    username = input("Masukkan username: ")
+    password = input("Masukkan password: ")
+    
+    if user == username and sandi == password:
+        print("Login berhasil!")
+    else:
+        print("Username atau password salah.")
+        menu()
+
+    jadwal = input('Masukan hari yang ingin anda cek : ')
+    if jadwal == 'senin':
+        senin()
+        pass
+    elif jadwal == 'rabu':
+        selasa()
+        pass
+    elif jadwal == 'rabu':
+        rabu()
+        pass
+    elif jadwal == 'Kamis':
+        kamis()
+        pass
+    elif jadwal == 'jukmat':
+        jukmat()
+        pass
+    elif jadwal == 'sabtu':
+        sabtu()
+        pass
+    elif jadwal == 'minggu':
+        minggu()
+        print('kode yang anda masukan kurang akurat!!')
+        pass
+
+def senin():
+    list = ['Pbo','Basis data','Baisi data 1']
+    for i in list:
+        print(i)
+    dosen = input('apakah anda ingin melihat dosen yang mengajar?..')
+    if dosen == 'y':
+        nama_dosen = ['Arul.S,kom.M,kom','Haswar.S,kom.M,kom']
+        pass
+        for j in nama_dosen:
+            print(j)
+    elif dosen == 'n':
+        pass
+
+def selasa():
+    mk = ['Java','Pengembangan multi media']
+    for i in mk:
+        print(i)
+    dosen = input('apakah anda ingin melihat dosen yang mengajar?..')
+    if dosen == 'y':
+        nama_dosen = ['deni.S,kom.M,kom','joni.S,kom.M,kom']
+        pass
+        for j in nama_dosen:
+            print(j)
+    elif dosen == 'n':
+        pass
+    
+def rabu():
+    mk = ['Python','Pemrograman web']
+    for i in mk:
+        print(i)
+    dosen = input('apakah anda ingin melihat dosen yang mengajar?..')
+    if dosen == 'y':
+        nama_dosen = ['Putu.S,kom.M,kom','Fyan.S,kom.M,kom']
+        pass
+        for j in nama_dosen:
+            print(j)
+    elif dosen == 'n':
+        pass
+    
+def kamis():
+    mk = ['Multi media','Dasar dasar pemrograman']
+    for i in mk:
+        print(i)
+        pass
+    dosen = input('apakah anda ingin melihat dosen yang mengajar?..')
+    if dosen == 'y':
+        nama_dosen = ['Aco,kom.M,kom','Tono.S,kom.M,kom']
+        for j in nama_dosen:
+            print(j)
+    elif dosen == 'n':
+        pass
+    
+def jukmat():
+    mk = ['Aljabar']
+    for i in mk:
+        print(i)
+        pass
+    dosen = input('apakah anda ingin melihat dosen yang mengajar?..')
+    if dosen == 'y':
+        nama_dosen = ['Artini.S,kom.M,kom']
+        for j in nama_dosen:
+            print(j)
+    elif dosen == 'n':
+        pass
+    
+def sabtu():
+    mk = ['Matematika dasar','C++']
+    for i in mk:
+        print(i)
+        pass
+    dosen = input('apakah anda ingin melihat dosen yang mengajar?..')
+    if dosen == 'y':
+        nama_dosen = ['Rini,kom.M,kom','Akmal.S,kom.M,kom']
+        for j in nama_dosen:
+            print(j)
+    elif dosen == 'n':
+        pass
+        
+def minggu():
+    print('libur')
+    
+
+menu()
+
+            
